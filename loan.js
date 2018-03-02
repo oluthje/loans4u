053e9fff25680e15b5b9530eae6951b0ba5b1084
Chyme <rukessel@gmail.com>
2018-03-02 08:10:37 -0800

    edited UI

--- loan.js
+++ loan.js
@@ -3,10 +3,8 @@ var output;
 function CreateDebt(){
  var Date1 = date();
  alert(Date1);
- var First_Name = document.getElementById("First_Name");
- var Last_Name = document.getElementById("Last_Name");
- var Endebted1 = document.getElementById("Endebted1");
- var Endebted2 = document.getElementById("Endebted2");
+ var Name = document.getElementById("First_Name");
+ var EndebtedTo = document.getElementById("Endebted1");
  var Debt_Amount = document.getElementById("Debt_Amount");
  var Interest_Rate = document.getElementById("Interest_Rate");
  var Interest_Period = document.getElementById("Interest_Period");
