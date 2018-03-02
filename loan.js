2623d5180f50ff580454a74b05d5a5eb3ad101ee
Chyme <rukessel@gmail.com>
2018-03-02 09:28:50 -0800

    started to add firebase code

--- loan.js
+++ loan.js
@@ -1,13 +1,24 @@
 var output;
 
+    console.log(firebase);
+
+    var indebtedTo = firebase.database().ref("IndebtedTo/");
+
+
+indebtedTo.set ({
+   Bill: {
+      number: 1,
+      Debt: 50.0,
+   },
+	
+   Amanda: {
+      number: 2,
+      Debt: 15.0,
+   }
+});
+    console.log(indebtedTo);
+
 function CreateDebt(){
- var Date1 = date();
- alert(Date1);
- var Name = document.getElementById("First_Name");
- var EndebtedTo = document.getElementById("Endebted1");
- var Debt_Amount = document.getElementById("Debt_Amount");
- var Interest_Rate = document.getElementById("Interest_Rate");
- var Interest_Period = document.getElementById("Interest_Period");
 }
 
 function date(){
